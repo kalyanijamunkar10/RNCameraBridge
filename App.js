ab7731028c89e5016e8d91e745f084bffffaefeb
kalyani <kalyani@gatha.co.in>
2021-02-12 13:29:00 +0530

    trying

--- App.js
+++ App.js
@@ -9,16 +9,28 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import {Button} from 'react-native';
-import {NativeModules} from 'react-native';
+import {
+  NativeModules,
+  NativeEventEmitter,
+  DeviceEventEmitter,
+} from 'react-native';
 const {CameraModule} = NativeModules;
 export default class App extends Component {
   _openCamera() {
     CameraModule.onClick();
   }
+  _sendEvent() {
+    const eventEmitter = new NativeEventEmitter(CameraModule.sendEvent);
+    var abc = eventEmitter.addListener('customEventName', (event) => {
+      console.log('eee', event)// "someValue"
+   });
+   console.log(abc);
+  }
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Button onPress={this._openCamera} title="Open Camera" />
+        <Button onPress={this._sendEvent} title="send Event" />
       </View>
     );
   }
--- android/app/src/main/java/com/rncamerabridge/CameraModule.java
+++ android/app/src/main/java/com/rncamerabridge/CameraModule.java
@@ -5,16 +5,24 @@ import android.content.Intent;
 import android.provider.MediaStore;
 import android.util.Log;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import javax.annotation.Nullable;
 
 public class CameraModule extends ReactContextBaseJavaModule {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     Activity activity;
+    ReactContext context;
     public CameraModule(ReactApplicationContext reactContext){
         super(reactContext);
         this.activity = reactContext.getCurrentActivity();
+        context = reactContext;
     }
 
     @Override
@@ -29,5 +37,14 @@ public class CameraModule extends ReactContextBaseJavaModule {
         if(cameraIntent.resolveActivity(context.getPackageManager()) !=null) {
             context.startActivity(cameraIntent);
         }
+    }
+
+    @ReactMethod
+    public void sendEvent(){
+        WritableMap params = Arguments.createMap();
+        params.putString("eventProperty", "someValue");
+
+        getReactInstanceManager().getReactApplicationContext().getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("customEventName",params);
     }
 }
